77fe4d3544463268132534fe92c65413b1c6e77b
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2024-07-29 12:56:37 +0100

    feat: add past events

--- app/past-events/page.tsx
+++ app/past-events/page.tsx
@@ -1,18 +1,59 @@
 "use client";
 
-import { Container, Text } from "@chakra-ui/react";
+import { Container, SimpleGrid, Text, Image } from "@chakra-ui/react";
 
 export default function PastEvents() {
+  const images23 = Array.from(
+    { length: 8 },
+    (_, i) => `/images/devfest23/${i + 1}.jpg`
+  );
+
+  const images22 = Array.from(
+    { length: 8 },
+    (_, i) => `/images/devfest22/${i + 1}.jpg`
+  );
+
   return (
     <Container maxW="1920px" px="20px" py="5vh">
       <Text sx={styles.heading}>DevFest Ireland 2023 - Dublin</Text>
       <Text fontSize="2xl" my="20px">
-        More info coming soon!
+        DevFest Ireland 2023 taken place in Griffith College, Dublin on 2nd
+        December 2023. It was a great success with a packed agenda of talks,
+        codelabs, and workshops across 2 stages, with subjects such as AI &
+        Machine Learning, Web, Mobile, Cloud & DEI.
       </Text>
+      <SimpleGrid
+        columns={{ base: 2, lg: 4 }}
+        spacing={10}
+        gridTemplateRows="masonry"
+        mb="20px">
+        {images23.map((src, index) => (
+          <Image
+            key={index}
+            src={src}
+            alt={`DevFest Ireland 2023 Image ${index + 1}`}
+          />
+        ))}
+      </SimpleGrid>
       <Text sx={styles.heading}>DevFest Ireland 2022 - Galway</Text>
-      <Text fontSize="2xl" mt="20px">
-        More info coming soon!
+      <Text fontSize="2xl" my="20px">
+        DevFest Ireland 2022 taken place in Genesys, Galway on 26th November
+        2022. As the first All Ireland DevFest, it was well received and formed
+        a great foundation for future events.
       </Text>
+      <SimpleGrid
+        columns={{ base: 2, lg: 4 }}
+        spacing={10}
+        gridTemplateRows="masonry"
+        mb="20px">
+        {images22.map((src, index) => (
+          <Image
+            key={index}
+            src={src}
+            alt={`DevFest Ireland 2023 Image ${index + 1}`}
+          />
+        ))}
+      </SimpleGrid>
     </Container>
   );
 }
